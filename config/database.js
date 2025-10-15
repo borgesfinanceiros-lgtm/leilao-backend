481ff4a3ec480498749ff27ede76608f7a4fe56c
borgesfinanceiros-lgtm <borgesfinanceiros@gmail.com>
2025-10-15 14:09:49 -0300

    Add files via upload
    Adicao do codigo Node.js

--- config/database.js
+++ config/database.js
@@ -1,19 +1,17 @@
 // config/database.js
 
-// Certifique-se de instalar 'dotenv' e criar um arquivo .env
+// Verifica se estamos em ambiente de produção (RENDER)
+// Se 'process.env.NODE_ENV' for 'production' (o padrão do Render),
+// ele usa as variáveis de ambiente. Caso contrário, ele usaria o modo local.
 
-require('dotenv').config(); // <--- AGORA DESCOMENTADO
-
-const dbConfig = {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '', // O fallback para senha vazia está CORRETO
-    database: process.env.DB_NAME || 'leilao_online_db',
-    port: process.env.DB_PORT || 3306,
-    
+const config = {
+    host: process.env.DB_HOST || 'localhost', // Se não achar a variável, usa localhost (segurança)
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 };
 
-module.exports = dbConfig;
+module.exports = config;
